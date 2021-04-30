a9e677952e6b0c8a39bb067ba3485ed7a638a5a8
QAS Operator <xf07bm@xf07bm-ws1.nsls2.bnl.local>
2021-04-30 11:06:45 -0400

    FIX: improve how shutter buttons are implemented
    Use obj.read() to get their current state, fixes fs button at QAS
    
    Co-authored-by: Caswell, Thomas <tcaswell@bnl.gov>

--- isstools/widgets/widget_beamline_status.py
+++ isstools/widgets/widget_beamline_status.py
@@ -8,6 +8,30 @@ from isstools.dialogs import UpdateUserDialog
 
 ui_path = pkg_resources.resource_filename('isstools', 'ui/ui_beamline_status.ui')
 
+def get_state(shutter):
+    """Get the state of the shutters in a uniform way.
+    
+    This assumes that:
+        - shutter.read() returns exactly 1 reading
+        - that reading is the state of the shutter
+
+
+    We need this function because the shutters at QAS have significant
+    non-uniformity in their implementation / nameing conventions.
+
+    Parameters
+    ----------
+    shutter : Device
+        An ophyd device that you claim is a shutter.
+
+    Returns
+    -------
+    str
+        The state of the shutter as a string.
+    """
+    return list(shutter.read().values())[0]['value']
+
+
 
 class UIBeamlineStatus(*uic.loadUiType(ui_path)):
     shutters_sig = QtCore.pyqtSignal()
@@ -20,7 +44,7 @@ class UIBeamlineStatus(*uic.loadUiType(ui_path)):
         # Initialize Ophyd elements
         self.shutters_sig.connect(self.change_shutter_color)
         self.shutters = shutters
-
+        self.color_map = {'Open': 'lime', 'Close': 'red', 'Not Open': 'red'}
         self.shutters_buttons = []
         for key, item in self.shutters.items():
             self.shutter_layout = QtWidgets.QVBoxLayout()
@@ -44,24 +68,24 @@ class UIBeamlineStatus(*uic.loadUiType(ui_path)):
             if hasattr(item, 'status') and hasattr(item.status, 'subscribe'):
                 item.button = button
                 item.status.subscribe(self.update_shutter)
+               
 
                 def toggle_shutter_call(shutter):
+                    
                     def toggle_shutter():
-                        if shutter.status.value == 'Open':
-                            st = shutter.set('Close')
-                        else:
-                            st = shutter.set('Open')
-                        while not st.done:
-                            time.sleep(0.01)
+                        state = get_state(shutter)
+                        toggle_map = {'Open': "Close",
+                            "Close": "Open",
+                            "Not Open": "Open"}
+                        shutter.set(toggle_map[state]).wait(timeout=1)
+                        
                     return toggle_shutter
 
                 button.clicked.connect(toggle_shutter_call(item))
-
-                if item.status.value == 'Open':
-                    button.setStyleSheet("background-color: lime")
-                else:
-                    button.setStyleSheet("background-color: red")
-
+                state = get_state(item)
+          
+                button.setStyleSheet(f"background-color: {self.color_map[state]}")
+          
         if self.horizontalLayout_shutters.count() <= 1:
             self.groupBox_shutters.setVisible(False)
 
@@ -69,11 +93,9 @@ class UIBeamlineStatus(*uic.loadUiType(ui_path)):
         if 'obj' in kwargs.keys():
             if hasattr(kwargs['obj'].parent, 'button'):
                 self.current_button = kwargs['obj'].parent.button
-                if kwargs['obj'].value == 'Open':
-                    self.current_button_color = 'lime'
-                else:
-                    self.current_button_color = 'red'
-
+                state = get_state(kwargs['obj'])
+                self.current_button_color = self.color_map[state]
+                
                 self.shutters_sig.emit()
 
     def change_shutter_color(self):
